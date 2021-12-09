c4783d544385079283f6ccfe03357234c6eef4f6
Mateus Maia <maiamateusotavio@gmail.com>
2021-12-08 23:45:07 -0300

    Listando categorias

--- src/services/domain/CategoriasService.ts
+++ src/services/domain/CategoriasService.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
 import { CategoriaDTO } from "../../models/categoria.dto";
 
