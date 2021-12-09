a5eba8a34a5bf504381a02520353fece8747867e
Mateus Maia <maiamateusotavio@gmail.com>
2021-12-08 23:29:12 -0300

    Ajustando clase CategoriasService

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriasService } from '../../services/domain/CategoriasService';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,11 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriasService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll().subscribe(response => {
+      console.log(response);
+    },
+      error => {
+        console.log(error);
+      })
   }
 
 }
