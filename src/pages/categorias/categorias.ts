c13c758e3a34659ecef15a8dd69ee0ce64cc8d18
Mateus Maia <maiamateusotavio@gmail.com>
2022-01-03 22:30:30 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriasService } from '../../services/domain/CategoriasService';
 
 /**
@@ -16,13 +18,17 @@ import { CategoriasService } from '../../services/domain/CategoriasService';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriasService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoriasPage');
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
       error => {
         console.log(error);
